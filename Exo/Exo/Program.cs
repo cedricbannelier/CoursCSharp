14818d4465feaaecd4f95db35c4996fda8991d9e
CCI Cedric <cedric.bannelier@gmail.com>
2017-12-15 16:44:48 +0100

    Ajout dexercice

---
+++ Exo/Exo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo
+{
+    class Program
+    {
+
+        static int aleatoire()
+        {
+            Random rnd1 = new Random();
+            return rnd1.Next(0, 2);
+        }
+        static void Main(string[] args)
+        {
+
+            //Variable des PV
+            int pvJoueur = 0;
+            int pvOrdi = 0;
+            int rejouer = 0;
+
+            do
+            {
+                while (pvJoueur < 5 && pvOrdi < 5)
+                {
+                    //On demande le choix du joueur
+                    Console.WriteLine("Choisir --> 0 : pile ou 1 : face");
+                    string choixJoueurString = Console.ReadLine();
+
+                    //Conversion d'un string vers int
+                    int choixJoueur = Int32.Parse(choixJoueurString);
+
+                    //Génération d'un nombre entre 0 et 2
+                    int choixOrdi = aleatoire();
+                    int jetDePiece = aleatoire();
+
+                    //Affiche choix joueur et ordi
+                    Console.WriteLine("Choix jouer {0} \n Choix ordi {1} \n Choix jetDePiece {2} \n", choixJoueur, choixOrdi, jetDePiece);
+                    Console.WriteLine("Score jouer {0} \n Score ordi {1} \n", pvJoueur, pvOrdi);
+
+                    if (choixJoueur == jetDePiece && choixOrdi == jetDePiece)
+                    {
+                        Console.WriteLine("Egalité \n");
+                    }
+                    else if (choixJoueur == jetDePiece)
+                    {
+                        Console.WriteLine("Joueur gagne \n");
+                        pvJoueur++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ordi gagne \n");
+                        pvOrdi++;
+                    }
+                }
+
+                if (pvJoueur == 6)
+                {
+                    Console.WriteLine("*** Joueur gagne la manche ***");
+                }
+                else
+                {
+                    Console.WriteLine("*** Ordi gagne la manche ***");
+
+                }
+                Console.WriteLine("Voulez-vous rejouer ? 0 : Non ou 1 : Oui");
+                string rejouerString = Console.ReadLine();
+                rejouer = Int32.Parse(rejouerString);
+                pvJoueur = 0;
+                pvOrdi = 0;
+
+            } while (rejouer == 1);
+
+            //            Console.ReadKey();
+        }
+    }
+}
---
+++ Exo2/Exo2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo2
+{
+    class Program
+    {
+        class Personnage
+        {
+            public string _pseudo;
+            public int _pointDeVie;
+            public int _pointDeDegat;
+            public int _valeurDattaque;
+
+            public void InitPersonnage()
+            {
+                this._pointDeVie = 20;
+                this._pointDeDegat = 4;
+                this._valeurDattaque = 60;
+            }
+            public void PrendreDesDegats(int degat)
+            {
+                this._pointDeVie = this._pointDeVie - degat;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Random rnd1 = new Random();
+
+            //Demande le pseudo
+            Console.WriteLine("Choisir votre pseudo du personne");
+            string pseudo = Console.ReadLine();
+
+            //Instance de la class Personnage (Objet)
+            Personnage personnagePrincipal = new Personnage();
+
+            //Affecte la valer de pseudo au personnageprincipal dans _pseudo
+            personnagePrincipal._pseudo = pseudo;
+
+            //Appel de la méthode pour init les caractérisque
+            personnagePrincipal.InitPersonnage();
+
+            //Affiche les caractéristiques du personnage
+            Console.WriteLine("Nom du personnage : {0}\nPoint de vie : {1}", personnagePrincipal._pseudo, personnagePrincipal._pointDeVie);
+            Console.WriteLine("Point de dégat : {0}\nValeur d'attaque : {1}", personnagePrincipal._pointDeDegat, personnagePrincipal._valeurDattaque);
+
+            Personnage personnageEnemie = new Personnage();
+            personnageEnemie._pseudo = "Adrien";
+            personnageEnemie.InitPersonnage();
+
+            int jetDe = rnd1.Next(0, 101);
+
+            do
+            {
+                jetDe = rnd1.Next(0, 101);
+                if (jetDe <= personnagePrincipal._valeurDattaque)
+                {
+                    personnageEnemie.PrendreDesDegats(personnagePrincipal._pointDeDegat);
+                    Console.WriteLine("{0} a touché {1}, le jet de dés a fait {2} ",personnagePrincipal._pseudo, personnageEnemie._pseudo, jetDe);
+                    Console.WriteLine("{0} il te reste {1}", personnageEnemie._pseudo, personnageEnemie._pointDeVie);
+                }
+                else
+                {
+                    Console.WriteLine("{0} a loupé {1}", personnagePrincipal._pseudo, personnageEnemie._pseudo);
+                }
+                Console.ReadKey();
+                if (personnageEnemie._pointDeVie<=0)
+                {
+                    break;
+                }
+
+                jetDe = rnd1.Next(0, 101);
+                if (jetDe <= personnageEnemie._valeurDattaque)
+                {
+                    personnagePrincipal.PrendreDesDegats(personnageEnemie._pointDeDegat);
+                    Console.WriteLine("{0} a touché {1}, le jet de dés a fait {2} ", personnageEnemie._pseudo, personnagePrincipal._pseudo, jetDe);
+                    Console.WriteLine("{0} il te reste {1}", personnagePrincipal._pseudo, personnagePrincipal._pointDeVie);
+                }
+                else
+                {
+                    Console.WriteLine("{1} a loupé {0}", personnagePrincipal._pseudo, personnageEnemie._pseudo);
+                }
+                Console.ReadKey();
+            } while (!(personnageEnemie._pointDeVie <= 0 || personnagePrincipal._pointDeVie <=0));
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
